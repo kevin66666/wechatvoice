1c32ae82cd0090902b7c19095d70396bf7bc14f7
zhangrui <zhangrui@wboll.com>
2016-08-27 21:54:52 +0800

    A

--- model/chuli.go
+++ model/chuli.go
@@ -27,7 +27,7 @@ func GetLawerInfoById(userId string) ([]map[string][]byte, error) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	sql := "select * from userinfo lawyer where userID =?"
+	sql := "select * from  lawyer where userID =?"
 	res, err1 := engin.Query(sql, userId)
 	if err1 != nil {
 		fmt.Println(err1)

b873ae8a9e7d61481d71476561897a43089079bc
zhangrui <zhangrui@wboll.com>
2016-08-28 11:18:01 +0800

    A

--- handler/question_front.go
+++ handler/question_front.go
@@ -2570,6 +2570,7 @@ func PayPeekAnswer(ctx *macaron.Context) string {
 	payF, _ := strconv.ParseFloat(pays, 64)
 	payF = payF / 100
 	payFs := strconv.FormatFloat(payF, 'f', 2, 64)
+	fmt.Println(payFs)
 	sign, prepayId, sings, signErr := PayBill(nstr, nSt, openId, orderNumber, "1", tStr)
 	fmt.Println(sings)
 	if signErr != nil {
